a70b0d0d3d5ed9c8d4168156eeecac6762be679c
faruqfadh <Faruq@ruangguru.com>
2022-08-09 21:07:46 +0700

    fix import

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/faruqfadhil/backend-golang-bootcamp/indonesia"
+	"github.com/faruqfadhil/backend-golang-bootcamp/helloworld/indonesia"
 )
 
 func main() {
