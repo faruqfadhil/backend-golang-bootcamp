3482b6b74d91ce14ddcca9757f74d96dc50eae6a
faruqfadh <Faruq@ruangguru.com>
2022-08-10 21:00:19 +0700

    add helloworld

---
+++ helloworld/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"helloworld/subfolder"
+)
+
+func main() {
+	subfolder.Hello()
+	fmt.Println("hello")
+}
---
+++ helloworld/subfolder/subfolder.go
@@ -0,0 +1,7 @@
+package subfolder
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("hello")
+}
